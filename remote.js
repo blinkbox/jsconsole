fd6146bfa8af1c14689b171509fa410a46394c26
Nisheeth Kashyap <nisheeth@blinkbox.com>
2012-08-02 10:37:54 +0100

    added some try catch to handle errors

--- remote.js
+++ remote.js
@@ -5,123 +5,140 @@
     // function getLastChild(el) {
     //   return (el.lastChild && el.lastChild.nodeName != '#text') ? getLastChild(el.lastChild) : el;
     // }
-
-    function getRemoteScript() {
-        var scripts = document.getElementsByTagName('script'),
-            remoteScript = scripts[scripts.length - 1];
-        for (var i = 0; i < scripts.length; i++) {
-            if (/jsconsole\..*(:\d+)?\/remote.js/.test(scripts[i].src)) {
-                remoteScript = scripts[i];
-                break;
-            }
-        }
-
-        return remoteScript;
-    }
-
-    var last = getRemoteScript();
-
-    // if (last.getAttribute('id') == '_firebugConsole') { // if Firebug is open, this all goes to crap
-    //   last = last.previousElementSibling;
-    // } 
-
-    var lastSrc = last.getAttribute('src'),
-        id = lastSrc.replace(/.*\?/, ''),
-        origin = 'http://' + lastSrc.substr(7).replace(/\/.*$/, ''),
-        remoteWindow = null,
-        queue = [],
-        msgType = '';
-
-    var remoteFrame = document.createElement('iframe');
-    remoteFrame.style.display = 'none';
-    remoteFrame.src = origin + '/remote.html?' + id;
-
-    remoteFrame.onload = function () {
-        remoteWindow = remoteFrame.contentWindow;
-        remoteWindow.postMessage('__init__', origin);
-        remoteWindow.postMessage(console.stringify({
-            response: 'Connection established with ' + window.location.toString() + '\n' + navigator.userAgent,
-            type: 'info'
-        }), origin);
-
-        for (var i = 0, ii = queue.length; i < ii; i++) {
-            remoteWindow.postMessage(queue[i], origin);
-        }
-    };	
-	
-    // this is new - in an attempt to allow this code to be included in the head element
-    document.documentElement.appendChild(remoteFrame);
-
-	
-	function sendToServer(funName, values, stack){
-		var msg = JSON.stringify({
-				response: values,
-                cmd: 'console.'+funName+'()',
-				type : funName,
-				stack : stack
-			});
-		if (remoteWindow) {
-			remoteWindow.postMessage(msg, origin);
-		} else {
-			queue.push(msg);
-		}
-	};
-
-	//init
-	window.console.connectTo(sendToServer);
-	
-	if(window.addEventListener){
-		window.addEventListener('message', function (event) {
-			if (event.origin != origin) return;
-			// eval the event.data command
-			try {
-				if (event.data.indexOf('console.log') === 0) {
-					eval('remote.echo(' + event.data.match(/console.log\((.*)\);?/)[1] + ', "' + event.data + '", true)');
-				} else {
-					remote.echo(eval(event.data), event.data, undefined); // must be undefined to work
+	try{
+		function getRemoteScript() {
+			var scripts = document.getElementsByTagName('script'),
+				remoteScript = scripts[scripts.length - 1];
+			for (var i = 0; i < scripts.length; i++) {
+				if (/jsconsole\..*(:\d+)?\/remote.js/.test(scripts[i].src)) {
+					remoteScript = scripts[i];
+					break;
 				}
-			} catch (e) {
-				console.error(e, event.data);
 			}
-		}, false);
+
+			return remoteScript;
+		}
+
+		var last = getRemoteScript();
+
+		// if (last.getAttribute('id') == '_firebugConsole') { // if Firebug is open, this all goes to crap
+		//   last = last.previousElementSibling;
+		// } 
+
+		var lastSrc = last.getAttribute('src'),
+			id = lastSrc.replace(/.*\?/, ''),
+			origin = 'http://' + lastSrc.substr(7).replace(/\/.*$/, ''),
+			remoteWindow = null,
+			queue = [],
+			msgType = '';
+
+		var remoteFrame = document.createElement('iframe');
+		remoteFrame.style.display = 'none';
+		remoteFrame.src = origin + '/remote.html?' + id;
+
+		remoteFrame.onload = function () {
+			try{
+				remoteWindow = remoteFrame.contentWindow;
+				remoteWindow.postMessage('__init__', origin);
+				remoteWindow.postMessage(console.stringify({
+					response: 'Connection established with ' + window.location.toString() + '\n' + navigator.userAgent,
+					type: 'info'
+				}), origin);
+
+				for (var i = 0, ii = queue.length; i < ii; i++) {
+					remoteWindow.postMessage(queue[i], origin);
+				}
+			}catch(e){
+				console.logToUI(e.message);
+			}
+		};	
 		
-		window.addEventListener('error', function (event) {
-			console.error(event);
-		}, false);
+		// this is new - in an attempt to allow this code to be included in the head element
+		document.documentElement.appendChild(remoteFrame);
+
+		
+		function sendToServer(funName, values, stack){
+			try{
+				var msg = JSON.stringify({
+						response: values,
+						cmd: 'console.'+funName+'()',
+						type : funName,
+						stack : stack
+					});
+				if (remoteWindow) {
+					remoteWindow.postMessage(msg, origin);
+				} else {
+					queue.push(msg);
+				}
+			}catch(e){
+				console.logToUI(e.message);
+			}			
+		};
+
+		//init
+		window.console.connectTo(sendToServer);
+		
+		if(window.addEventListener){
+			window.addEventListener('message', function (event) {
+				if (event.origin != origin) return;
+				// eval the event.data command
+				try {
+					if (event.data.indexOf('console.log') === 0) {
+						eval('remote.echo(' + event.data.match(/console.log\((.*)\);?/)[1] + ', "' + event.data + '", true)');
+					} else {
+						remote.echo(eval(event.data), event.data, undefined); // must be undefined to work
+					}
+				} catch (e) {
+					console.error(e, event.data);
+				}
+			}, false);
+			
+			window.addEventListener('error', function (event) {
+				console.error(event);
+			}, false);
+		}
+
+		
+		var remote = {
+			echo: function () {
+				try{
+					var args = [].slice.call(arguments, 0),
+						plain = args.pop(),
+						cmd = args.pop(),
+						response = args;
+
+					var argsObj = console.stringify(response, plain),
+						msg = JSON.stringify({
+							response: argsObj,
+							cmd: cmd
+						});
+					if (remoteWindow) {
+						remoteWindow.postMessage(msg, origin);
+					} else {
+						queue.push(msg);
+					}
+				}catch(e){
+					console.logToUI(e.message);
+				}				
+			}
+		};
+		
+		window.remote = remote;
+
+		function warnUsage() {
+			var useSS = false;
+			try {
+				sessionStorage.getItem('foo');
+				useSS = true;
+			} catch (e) {}
+			
+			if (!(useSS ? sessionStorage.jsconsole : window.name)) {
+				if (useSS) sessionStorage.jsconsole = 1;
+				else window.name = 1;
+			}
+		}
+	}catch(e){
+		console.logToUI(e.message);
 	}
-
-	
-    var remote = {
-        echo: function () {
-            var args = [].slice.call(arguments, 0),
-                plain = args.pop(),
-                cmd = args.pop(),
-                response = args;
-
-            var argsObj = console.stringify(response, plain),
-                msg = JSON.stringify({
-                    response: argsObj,
-                    cmd: cmd
-                });
-            if (remoteWindow) {
-                remoteWindow.postMessage(msg, origin);
-            } else {
-                queue.push(msg);
-            }
-        }
-    };
-
-    function warnUsage() {
-        var useSS = false;
-        try {
-            sessionStorage.getItem('foo');
-            useSS = true;
-        } catch (e) {}
-        if (!(useSS ? sessionStorage.jsconsole : window.name)) {
-            if (useSS) sessionStorage.jsconsole = 1;
-            else window.name = 1;
-        }
-    }
-
-    warnUsage();
+    //warnUsage();
 })();
